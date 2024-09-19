dd2714c961bb7fb548aea04a791506bc3bf3fbe8
Björn Preus <bjorn.preus@simac.com>
2024-09-19 21:05:35 +0200

    Add Get-XelionTrunks function to retrieve trunks from Xelion API

---
+++ xelionmgmt/functions/Get-XelionTrunks.ps1
@@ -0,0 +1,36 @@
+function Get-XelionTrunks {
+	param (
+        [Parameter(Mandatory = $true)]
+        [string] $Serverfqdn,
+
+        [Parameter(Mandatory = $true)]
+        [string] $Xeliontenant,
+
+        [Parameter(Mandatory = $false)]
+        [string] $oid
+	)
+    if ($oid) {
+        $uri = "https://$($Serverfqdn)/api/v1/$($Xeliontenant)/trunks/$($oid)"
+    } else {
+        $uri = "https://$($Serverfqdn)/api/v1/$($Xeliontenant)/trunks"
+    }
+	
+    # Send the GET request
+	try {
+        if (-not $authtoken) {
+            Write-Host "Authenticate to server by using Connect-Xelion to obtain authentication token"
+        } else {
+            $response = (Invoke-RestMethod -Uri $uri -Method Get -Headers @{Authorization = "xelion $authtoken"} -ContentType "application/json")
+            if ($oid) {
+                return $response.object
+            }
+            else {
+                return $response.data.object
+            }
+        }
+
+	} catch {
+		Write-Error "Error: Failed to authenticate to Xelion API: $_"
+		return $null
+	}
+}
